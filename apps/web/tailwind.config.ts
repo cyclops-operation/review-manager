66b0365fac0d743d0d78d03dd3e12ba1eec55244
p-acid <qkrtkstjd01@gmail.com>
2024-06-13 23:00:28 +0900

    chore: add global css and tailwind config setting

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -58,7 +58,32 @@ const config: Config = {
     }),
   ],
   daisyui: {
-    themes: ["forest"],
+    themes: [
+      {
+        mytheme: {
+          primary: "#047857",
+          "primary-content": "#d2e3db",
+          secondary: "#15803d",
+          "secondary-content": "#d4e5d6",
+          accent: "#4d7c0f",
+          "accent-content": "#d9e4d1",
+          neutral: "#0f766e",
+          "neutral-content": "#d3e3e0",
+          "base-100": "#18181B",
+          "base-200": "#27272A",
+          "base-300": "#3F3F46",
+          "base-content": "#cbcbcc",
+          info: "#06B6D4",
+          "info-content": "#000c10",
+          success: "#4ADE80",
+          "success-content": "#021206",
+          warning: "#EAB308",
+          "warning-content": "#130c00",
+          error: "#F87171",
+          "error-content": "#150404",
+        },
+      },
+    ],
   },
 }
 export default config
